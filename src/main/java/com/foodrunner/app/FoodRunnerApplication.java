c491eb1b92153381ccf401cd79b3eb236d159727
Codersonudiwakar <138717372+Codersonudiwakar@users.noreply.github.com>
2024-07-22 22:21:06 +0530

    commit

---
+++ src/main/java/com/foodrunner/app/FoodRunnerApplication.java
@@ -0,0 +1,13 @@
+package com.foodrunner.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodRunnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodRunnerApplication.class, args);
+	}
+
+}

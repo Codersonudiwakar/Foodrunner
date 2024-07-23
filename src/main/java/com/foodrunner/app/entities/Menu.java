24471b671e1b3caf0124c6430ba4e17ee5f06fae
Codersonudiwakar <138717372+Codersonudiwakar@users.noreply.github.com>
2024-07-23 23:18:07 +0530

    commit

--- src/main/java/com/foodrunner/app/entities/Menu.java
+++ src/main/java/com/foodrunner/app/entities/Menu.java
@@ -1,26 +1,24 @@
 package com.foodrunner.app.entities;
 
-import java.util.List;
-
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
-public class Restaurant {
+public class Menu {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    private String address;
-    private String phoneNumber;
-    private String email;
+    private String description;
+    private Double price;
 
-    @OneToMany(mappedBy = "restaurant", cascade = CascadeType.ALL)
-    private List<Menu> menus;
+    @ManyToOne
+    @JoinColumn(name = "restaurant_id")
+    private Restaurant restaurant;
 
     // getters and setters
 }

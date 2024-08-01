1ca04e8673d4639eb6d008c53b40fe1def531792
Codersonudiwakar <138717372+Codersonudiwakar@users.noreply.github.com>
2024-08-01 20:07:39 +0530

    commit

--- src/main/java/com/foodrunner/app/entities/Address.java
+++ src/main/java/com/foodrunner/app/entities/Address.java
@@ -12,6 +12,59 @@ public class Address {
     private String landmark;
     private String state;
     private long zip;
+    
+	public Address(Long addressId, String homeNo, String area, String city, String landmark, String state, long zip) {
+		super();
+		this.addressId = addressId;
+		this.homeNo = homeNo;
+		this.area = area;
+		this.city = city;
+		this.landmark = landmark;
+		this.state = state;
+		this.zip = zip;
+	}
+	public Long getAddressId() {
+		return addressId;
+	}
+	public void setAddressId(Long addressId) {
+		this.addressId = addressId;
+	}
+	public String getHomeNo() {
+		return homeNo;
+	}
+	public void setHomeNo(String homeNo) {
+		this.homeNo = homeNo;
+	}
+	public String getArea() {
+		return area;
+	}
+	public void setArea(String area) {
+		this.area = area;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getLandmark() {
+		return landmark;
+	}
+	public void setLandmark(String landmark) {
+		this.landmark = landmark;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public long getZip() {
+		return zip;
+	}
+	public void setZip(long zip) {
+		this.zip = zip;
+	}
 	
 
 }
